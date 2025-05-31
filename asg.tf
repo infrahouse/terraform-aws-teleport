6aedb70182dc064c1a0aa63c2bc0dd2e7ee5ce06
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2025-05-31 06:27:36 -0700

    Remove my ssh key
    it was used for development, not needed anymore.

--- asg.tf
+++ asg.tf
@@ -48,16 +48,11 @@ resource "aws_key_pair" "teleport" {
   tags            = local.default_module_tags
 }
 
-resource "aws_key_pair" "mediapc" {
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDpgAP1z1Lxg9Uv4tam6WdJBcAftZR4ik7RsSr6aNXqfnTj4civrhd/q8qMqF6wL//3OujVDZfhJcffTzPS2XYhUxh/rRVOB3xcqwETppdykD0XZpkHkc8XtmHpiqk6E9iBI4mDwYcDqEg3/vrDAGYYsnFwWmdDinxzMH1Gei+NPTmTqU+wJ1JZvkw3WBEMZKlUVJC/+nuv+jbMmCtm7sIM4rlp2wyzLWYoidRNMK97sG8+v+mDQol/qXK3Fuetj+1f+vSx2obSzpTxL4RYg1kS6W1fBlSvstDV5bQG4HvywzN5Y8eCpwzHLZ1tYtTycZEApFdy+MSfws5vPOpggQlWfZ4vA8ujfWAF75J+WABV4DlSJ3Ng6rLMW78hVatANUnb9s4clOS8H6yAjv+bU3OElKBkQ10wNneoFIMOA3grjPvPp5r8dI0WDXPIznJThDJO5yMCy3OfCXlu38VDQa1sjVj1zAPG+Vn2DsdVrl50hWSYSB17Zww0MYEr8N5rfFE= aleks@MediaPC"
-}
-
 resource "aws_launch_template" "teleport" {
   name_prefix   = "teleport-"
   instance_type = "t3a.micro"
-  # key_name      = aws_key_pair.teleport.key_name
-  key_name = aws_key_pair.mediapc.key_name
-  image_id = data.aws_ami.ubuntu_pro.id
+  key_name      = aws_key_pair.teleport.key_name
+  image_id      = data.aws_ami.ubuntu_pro.id
   iam_instance_profile {
     arn = module.instance_profile.instance_profile_arn
   }
