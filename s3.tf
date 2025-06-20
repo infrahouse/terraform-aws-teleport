ae51c2cfc274588d923df014579ceb0eb9cccb92
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2025-06-20 09:44:43 -0700

    Enforce TLS on accessing the teleport bucket (#15)

--- s3.tf
+++ s3.tf
@@ -30,3 +30,36 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_s3_bucket_policy" "teleport" {
+  bucket = aws_s3_bucket.teleport.id
+  policy = data.aws_iam_policy_document.enforce_ssl_policy.json
+}
+
+data "aws_iam_policy_document" "enforce_ssl_policy" {
+  statement {
+    sid    = "AllowSSLRequestsOnly"
+    effect = "Deny"
+
+    actions = [
+      "s3:*",
+    ]
+
+    resources = [
+      aws_s3_bucket.teleport.arn,
+      "${aws_s3_bucket.teleport.arn}/*",
+    ]
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+  }
+
+}
